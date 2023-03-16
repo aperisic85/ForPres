c0aae54d89d0d6c9eb269fd0d2cbda776ebd23f1
Ante Perisic <perisic.ante@gmail.com>
2023-03-16 11:02:54 +0100

    implement into_iter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 struct MyGarage(Vec<Cars>);
+#[derive(Debug)]
 struct Cars {
     name: String,
     car_type: CarType,
 }
-
+#[derive(Debug)]
 enum CarType {
     Sport,
     Road,
@@ -11,30 +12,37 @@ enum CarType {
 }
 
 impl Cars {
-    fn new(name: String, car_type: CarType) -> Cars{
-        Cars{name, car_type}
+    fn new(name: String, car_type: CarType) -> Cars {
+        Cars { name, car_type }
     }
 }
 
 impl MyGarage {
     fn new() -> Self {
-        
-        let mut myVec: Vec<Cars> = Vec::new();
-        MyGarage(myVec)
+        let my_vec: Vec<Cars> = Vec::new();
+        MyGarage(my_vec)
+    }
+}
+
+impl IntoIterator for MyGarage {
+    type Item = Cars;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
     }
 }
 
 fn main() {
-    let newCar = Cars::new(String::from("mercedes"), CarType::Sport);
-    let newCar2 = Cars::new(String::from("bmw"), CarType::Road);
-    let newCar3 = Cars::new(String::from("range rover"), CarType::Offroad);
-    let mut newGarage = MyGarage::new();
-    newGarage.0.push(newCar);
-    newGarage.0.push(newCar2);
-    newGarage.0.push(newCar3);
+    let new_car = Cars::new(String::from("mercedes"), CarType::Sport);
+    let new_car2 = Cars::new(String::from("bmw"), CarType::Road);
+    let new_car3 = Cars::new(String::from("range rover"), CarType::Offroad);
+    let mut new_garage = MyGarage::new();
+    new_garage.0.push(new_car);
+    new_garage.0.push(new_car2);
+    new_garage.0.push(new_car3);
 
-    for x in MyGarage {
-        println!("{}",x);
+    for x in new_garage {
+        println!("{:?}", x.name);
     }
-
 }
